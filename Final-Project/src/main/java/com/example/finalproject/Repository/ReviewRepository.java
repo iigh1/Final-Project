b6fab8d47a4359d8333667d41cc9b1647628c02b
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-06-14 23:51:45 +0300

    Update ReviewRepository.java

--- Final-Project/src/main/java/com/example/finalproject/Repository/ReviewRepository.java
+++ Final-Project/src/main/java/com/example/finalproject/Repository/ReviewRepository.java
@@ -1,4 +1,22 @@
 package com.example.finalproject.Repository;
 
-public class ReviewRepository {
+import com.example.finalproject.Model.MyService;
+import com.example.finalproject.Model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review,Integer> {
+
+    Review findReviewById(Integer id);
+
+
+    @Query("select r from Review r where r.myService.provider.id = ?1")
+    List<Review> getReviewsByProvider(Integer id);
+
+
+
 }
