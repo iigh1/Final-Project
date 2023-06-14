477f4680d013b4cc4cc67c94ae7b30a97ea5dd2d
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-06-14 23:52:43 +0300

    Update ReviewController.java

--- Final-Project/src/main/java/com/example/finalproject/Controller/ReviewController.java
+++ Final-Project/src/main/java/com/example/finalproject/Controller/ReviewController.java
@@ -1,4 +1,48 @@
 package com.example.finalproject.Controller;
 
+import com.example.finalproject.DTO.ReviewDTO;
+import com.example.finalproject.Model.MyUser;
+import com.example.finalproject.Model.Review;
+import com.example.finalproject.Service.ReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/review")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping("/get")
+    public ResponseEntity getAll(){
+        return ResponseEntity.status(200).body(reviewService.getAll());
+    }
+
+    @GetMapping("/get-reviews/{providerId}")
+    public ResponseEntity getReviewsOfProvider(@AuthenticationPrincipal MyUser user, @PathVariable Integer providerId){
+        return ResponseEntity.status(200).body(reviewService.getReviewsOfProvider(user.getId(), providerId));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addReview(@AuthenticationPrincipal MyUser user, @Valid @RequestBody ReviewDTO dto){
+        reviewService.addReview(user, dto);
+        return ResponseEntity.status(200).body("Review added");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateReview(@AuthenticationPrincipal MyUser user, @Valid @RequestBody Review review, @PathVariable Integer id){
+        reviewService.updateReview(user, id, review);
+        return ResponseEntity.status(200).body("Review updated");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteReview(@AuthenticationPrincipal MyUser user, @PathVariable Integer id){
+        reviewService.deleteReview(user, id);
+        return ResponseEntity.status(200).body("Review deleted");
+    }
 }
