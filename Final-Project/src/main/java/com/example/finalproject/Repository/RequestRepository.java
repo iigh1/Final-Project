d1dea7c0e8160c0dcd1ab30100dfd4382ad74122
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-06-15 10:53:46 +0300

    Update RequestRepository.java

--- Final-Project/src/main/java/com/example/finalproject/Repository/RequestRepository.java
+++ Final-Project/src/main/java/com/example/finalproject/Repository/RequestRepository.java
@@ -1,4 +1,11 @@
 package com.example.finalproject.Repository;
 
-public interface RequestRepository {
+import com.example.finalproject.Model.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request,Integer> {
+
+    Request findRequestById(Integer id);
 }
