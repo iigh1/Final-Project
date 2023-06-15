1d8ebaac7bed84f734bfb276c701f834dcccf5f2
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-06-15 10:55:05 +0300

    Update RequestController.java

--- Final-Project/src/main/java/com/example/finalproject/Controller/RequestController.java
+++ Final-Project/src/main/java/com/example/finalproject/Controller/RequestController.java
@@ -1,4 +1,41 @@
 package com.example.finalproject.Controller;
 
+import com.example.finalproject.Model.MyUser;
+import com.example.finalproject.Model.Request;
+import com.example.finalproject.Service.RequestService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/request")
 public class RequestController {
+
+    private final RequestService requestService;
+
+    @GetMapping("/get")
+    public ResponseEntity getAll(){
+        return ResponseEntity.status(200).body(requestService.getAll());
+    }
+
+    @PostMapping("/add/{serviceId}")
+    public ResponseEntity addRequest(@AuthenticationPrincipal MyUser user, @Valid @RequestBody Request request, @PathVariable Integer serviceId){
+        requestService.addRequest(user, request, serviceId);
+        return ResponseEntity.status(200).body("Request added");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateRequest(@AuthenticationPrincipal MyUser user, @Valid @RequestBody Request request, @PathVariable Integer id){
+        requestService.updateRequest(user, request, id);
+        return ResponseEntity.status(200).body("Request updated");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteRequest(@AuthenticationPrincipal MyUser user,@PathVariable Integer id){
+        requestService.deleteRequest(user, id);
+        return ResponseEntity.status(200).body("Request deleted");
+    }
 }
