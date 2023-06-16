519cd5f9d9fa1b9b05cbfbdf168d6f0690cb0beb
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-06-16 16:51:37 +0300

    Update RequestService.java

--- Final-Project/src/main/java/com/example/finalproject/Service/RequestService.java
+++ Final-Project/src/main/java/com/example/finalproject/Service/RequestService.java
@@ -1,9 +1,11 @@
 package com.example.finalproject.Service;
 
 import com.example.finalproject.ApiException.ApiException;
+import com.example.finalproject.Model.Customer;
 import com.example.finalproject.Model.MyService;
 import com.example.finalproject.Model.MyUser;
 import com.example.finalproject.Model.Request;
+import com.example.finalproject.Repository.CustomerRepository;
 import com.example.finalproject.Repository.MyServiceRepository;
 import com.example.finalproject.Repository.RequestRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ public class RequestService {
 
     private final RequestRepository requestRepository;
     private final MyServiceRepository myServiceRepository;
+    private final CustomerRepository customerRepository;
 
     public List<Request> getAll(){
         return requestRepository.findAll();
@@ -24,7 +27,7 @@ public class RequestService {
 
     public void addRequest(MyUser user, Request request, Integer serviceId){
         MyService service = myServiceRepository.findMyServiceById(serviceId);
-        if (service == null || !user.getRole().equalsIgnoreCase("customer"))
+        if (service == null)
             throw new ApiException("Invalid");
         request.setCustomer(user.getCustomer());
         request.setStatus("New");
@@ -36,11 +39,10 @@ public class RequestService {
 
     public void updateRequest(MyUser user, Request request, Integer id){
         Request req = requestRepository.findRequestById(id);
-        if (req == null || req.getCustomer() != user.getCustomer())
+        if (req == null || req.getCustomer() != user.getCustomer() || !req.getStatus().equalsIgnoreCase("new"))
             throw new ApiException("Invalid");
         req.setDate(request.getDate());
         req.setOccasion(request.getOccasion());
-        req.setTime(request.getTime());
         requestRepository.save(req);
     }
 
@@ -49,5 +51,22 @@ public class RequestService {
         if (req == null || req.getCustomer() != user.getCustomer() || !req.getStatus().equalsIgnoreCase("new"))
             throw new ApiException("Invalid");
         requestRepository.delete(req);
+    }
+
+    public void cancelRequest(MyUser user, Integer id){
+        Request req = requestRepository.findRequestById(id);
+        if (req == null || req.getCustomer() != user.getCustomer() || !req.getStatus().equalsIgnoreCase("new"))
+            throw new ApiException("Invalid");
+        req.setStatus("Canceled");
+        requestRepository.save(req);
+    }
+
+    public void gift(MyUser user, Integer requestId, Integer customerID){
+        Request req = requestRepository.findRequestById(requestId);
+        Customer customer = customerRepository.findCustomerById(customerID);
+        if (req == null || customer == null || req.getCustomer() != user.getCustomer() || req.getStatus().equalsIgnoreCase("cancel"))
+            throw new ApiException("Invalid");
+        req.setCustomer(customer);
+        requestRepository.save(req);
     }
 }
