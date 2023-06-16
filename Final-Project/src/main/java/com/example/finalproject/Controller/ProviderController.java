c603792b1ae7e622daf693663716dabdd04f854d
Ghadeer <102277108+iigh1@users.noreply.github.com>
2023-06-16 15:34:00 +0300

    Update ProviderController.java

--- Final-Project/src/main/java/com/example/finalproject/Controller/ProviderController.java
+++ Final-Project/src/main/java/com/example/finalproject/Controller/ProviderController.java
@@ -16,25 +16,64 @@ public class ProviderController {
 
     private final ProviderService providerService;
 
-
     @GetMapping("/get")
     public ResponseEntity getAll(){
         return ResponseEntity.status(200).body(providerService.getAll());
     }
 
+    @GetMapping("/get-provider")
+    public ResponseEntity getProvider(@AuthenticationPrincipal MyUser myUser, Integer providerId){
+        return ResponseEntity.status(200).body(providerService.getProvider(myUser, providerId));
+    }
+
     @PostMapping("/register")
-    public ResponseEntity addProvider(@RequestBody ProviderDTO dto){
-          providerService.addProvider(dto);
+    public ResponseEntity addProvider(@Valid  @RequestBody ProviderDTO dto){
+        providerService.addProvider(dto);
         return ResponseEntity.status(200).body("provider register");
     }
-    @PutMapping("/updated/{id}")
-    public ResponseEntity updateProvider(@AuthenticationPrincipal MyUser myUser, @RequestBody Provider provider,@PathVariable Integer id){
-        providerService.updateProvider(myUser.getId(),provider,id);
+    @PutMapping("/update")
+    public ResponseEntity updateProvider(@AuthenticationPrincipal MyUser myUser,@Valid @RequestBody Provider provider){
+        providerService.updateProvider(myUser.getId(),provider);
         return ResponseEntity.status(200).body("provider updated");
     }
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity deleteProvider(@PathVariable Integer id){
-        providerService.deleteProvider(id);
+    @DeleteMapping("/delete")
+    public ResponseEntity deleteProvider(@AuthenticationPrincipal MyUser myUser){
+        providerService.deleteProvider(myUser.getId());
         return ResponseEntity.status(200).body("provider deleted");
+    }
+    @PutMapping("/discount/{amount}")
+    public ResponseEntity discount(@AuthenticationPrincipal MyUser myUser,@PathVariable  Integer amount){
+        providerService.discount(myUser,amount);
+        return ResponseEntity.status(200).body("discount successful");
+    }
+    @GetMapping("/get-request")
+    public ResponseEntity getRequest(@AuthenticationPrincipal MyUser myUser){
+       return ResponseEntity.status(200).body(providerService.getRequest(myUser));
+
+    }
+
+    @GetMapping("/get-service")
+    public ResponseEntity getService(@AuthenticationPrincipal MyUser myUser, Integer providerId){
+        return ResponseEntity.status(200).body(providerService.getService(myUser, providerId));
+    }
+
+    @GetMapping("/get-complete")
+    public ResponseEntity getCompletedRequest(@AuthenticationPrincipal MyUser myUser){
+        return ResponseEntity.status(200).body(providerService.getCompletedRequest(myUser));
+
+    }
+    @GetMapping("/get-new")
+    public ResponseEntity getNewRequest(@AuthenticationPrincipal MyUser myUser){
+        return ResponseEntity.status(200).body(providerService.getNewRequest(myUser));
+    }
+    @PutMapping("/accept-request/{requestId}")
+    public ResponseEntity acceptRequest(@AuthenticationPrincipal MyUser myUser,@PathVariable Integer requestId){
+        providerService.acceptRequest(myUser,requestId);
+        return ResponseEntity.status(200).body("accepted Request");
+    }
+    @PutMapping("/reject-request/{requestId}")
+    public ResponseEntity rejectRequest(@AuthenticationPrincipal MyUser myUser,@PathVariable Integer requestId){
+        providerService.acceptRequest(myUser,requestId);
+        return ResponseEntity.status(200).body("rejected Request");
     }
 }
