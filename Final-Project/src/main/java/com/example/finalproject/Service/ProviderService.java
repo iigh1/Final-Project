c7d2473fee1c9af2a459cc36ce7a37c1e898ed0d
Ghadeer <102277108+iigh1@users.noreply.github.com>
2023-06-16 15:32:09 +0300

    Update ProviderService.java

--- Final-Project/src/main/java/com/example/finalproject/Service/ProviderService.java
+++ Final-Project/src/main/java/com/example/finalproject/Service/ProviderService.java
@@ -65,5 +65,79 @@ public class ProviderService {
         }
         providerRepository.delete(oldProvider);
     }
+    public void discount(MyUser myUser, Integer amount){
+        Provider provider = myUser.getProvider();
+        if (provider==null){
+            throw new ApiException("invalid");
+        }
+        Set<MyService> services=myUser.getProvider().getServiceSet();
+        for ( MyService s :services){
+            s.setPrice(s.getPrice()-(s.getPrice()*amount/100));
+            myServiceRepository.save(s);
+        }
+    }
+
+    public Set<Request> getRequest(MyUser myUser){
+        Provider p =myUser.getProvider();
+        if (p==null){
+            throw new ApiException("invalid");
+        }
+        return p.getRequestSet();
+    }
+
+    public Set<MyService> getService(MyUser myUser, Integer providerId){
+        Provider p=myUser.getProvider();
+        if (myUser.getRole().equalsIgnoreCase("provider") || myUser.getId() != providerId){
+            throw new ApiException("invalid");
+        }
+        return p.getServiceSet();
+    }
+
+    public List<Request> getCompletedRequest(MyUser myUser){
+        Provider provider = myUser.getProvider();
+        if (provider==null){
+            throw new ApiException("invalid");
+        }
+        List<Request> requests = new ArrayList<>();
+        Set<Request> requestSet = provider.getRequestSet();
+        for (Request r : requestSet){
+            if (r.getStatus().equalsIgnoreCase("completed"));
+                requests.add(r);
+        }
+        return requests;
+    }
+    public List<Request> getNewRequest(MyUser myUser){
+        Provider provider = myUser.getProvider();
+        if (provider==null){
+            throw new ApiException("invalid");
+        }
+        List<Request> requests = new ArrayList<>();
+        Set<Request> requestSet = provider.getRequestSet();
+        for (Request r : requestSet){
+            if (r.getStatus().equalsIgnoreCase("new"));
+            requests.add(r);
+        }
+        return requests;
+    }
+
+    public void acceptRequest(MyUser myUser, Integer requestId){
+        Provider provider=myUser.getProvider();
+        Request r =  requestRepository.findRequestById(requestId);
+        if (provider==null || r==null || r.getProvider()!=provider){
+            throw new ApiException("invalid");
+        }
+        r.setStatus("accept");
+        requestRepository.save(r);
+    }
+
+    public void rejectRequest(MyUser myUser, Integer requestId){
+        Provider provider=myUser.getProvider();
+        Request r =  requestRepository.findRequestById(requestId);
+        if (provider==null || r==null || r.getProvider()!=provider){
+            throw new ApiException("invalid");
+        }
+        r.setStatus("reject");
+        requestRepository.save(r);
+    }
 
 }
