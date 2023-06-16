4a3701580e529ea4a4baff4e73ef64ffa0d3ca85
Ghadeer <102277108+iigh1@users.noreply.github.com>
2023-06-16 15:38:05 +0300

    Update ProviderDTO.java

--- Final-Project/src/main/java/com/example/finalproject/DTO/ProviderDTO.java
+++ Final-Project/src/main/java/com/example/finalproject/DTO/ProviderDTO.java
@@ -7,14 +7,12 @@ import lombok.Data;
 @AllArgsConstructor
 public class ProviderDTO {
 
-//    private Integer user_id;
     private String name;
     private String username;
     private String password;
     private String email;
     private String phoneNumber;
     private String field;
-//    private Double rating;
     private String instagramAccount;
-//    private Integer wallet;
+    
 }
