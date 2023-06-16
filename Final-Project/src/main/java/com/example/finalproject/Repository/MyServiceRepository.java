952b763542a1aa8e11684a24b0bae317e5a9764e
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-06-16 15:30:11 +0300

    Update MyServiceRepository.java

--- Final-Project/src/main/java/com/example/finalproject/Repository/MyServiceRepository.java
+++ Final-Project/src/main/java/com/example/finalproject/Repository/MyServiceRepository.java
@@ -1,7 +1,6 @@
 package com.example.finalproject.Repository;
 
 import com.example.finalproject.Model.MyService;
-import com.example.finalproject.Model.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,15 +12,11 @@ public interface MyServiceRepository extends JpaRepository<MyService,Integer> {
 
     MyService findMyServiceById (Integer id);
 
-    @Query("select s from MyService s where s.provider = ?1")
-    List<MyService> findMyServicesByProvider(Provider provider);
+    @Query("select s from MyService s where s.provider.id = ?1")
+    List<MyService> findMyServicesByProvider(Integer id);
 
+    @Query("select s from MyService s where s.Category = ?1")
     List<MyService> findMyServicesByCategory(String category);
 
-    @Query("select s from MyService s where s.price = ?1 or s.price > ?1")
-    List<MyService> findMyServicesByPrice(Double price);
-
-    @Query("select s from MyService s where s.rating = ?1 or s.rating > ?1")
-    List<MyService> findMyServicesByRating(Double rate);
 
 }
