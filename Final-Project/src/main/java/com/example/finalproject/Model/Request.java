15da3785e3613be8327137cd6e0d4df0146e8f12
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-06-16 16:51:11 +0300

    Update Request.java

--- Final-Project/src/main/java/com/example/finalproject/Model/Request.java
+++ Final-Project/src/main/java/com/example/finalproject/Model/Request.java
@@ -3,7 +3,6 @@ package com.example.finalproject.Model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -11,59 +10,52 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.Set;
-
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-    public class Request {
+public class Request {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-        private Date date;
+    @NotEmpty(message = "Date is required")
+    private String date;
 
-        @Column(name="time")
-        private LocalDateTime time;
+    @NotEmpty(message = "Occasion is required")
+    @Column(columnDefinition = "varchar(20)")
+    private String occasion;
 
-        @NotEmpty(message = "Occasion is required")
-        @Column(columnDefinition = "varchar(20)")
-        private String occasion;
+    @Pattern(regexp = "\\b(?:New|Accepted|Canceled|Completed)\\b",message = "Status Not Valid")
+    @Column(columnDefinition = "varchar(20) check(Status='New' or Status='Accepted' " +
+            "or Status='canceled' or Status='Completed')")
+    private String status;
 
-        @Pattern(regexp = "\\b(?:New|Accepted|inProgress|Completed)\\b",message = "Status Not Valid")
-        @Column(columnDefinition = "varchar(20) not null check(Status='New' or Status='Accepted' " +
-                "or Status='inProgress' or Status='Completed')")
-        private String status;
+    @Positive
+    private Double price;
 
-        @NotNull(message = "Price is required")
-        @Positive
-        private Double price;
-
-        @ManyToOne
-        @JoinColumn(name = "request_id",referencedColumnName = "id")
-        @JsonIgnore
-        private MyService myService;
+    @ManyToOne
+    @JoinColumn(name = "request_id",referencedColumnName = "id")
+    @JsonIgnore
+    private MyService myService;
 
 
-       @OneToOne(cascade = CascadeType.ALL,mappedBy = "request")
-       @PrimaryKeyJoinColumn
-       private Review review;
+    @OneToOne(cascade = CascadeType.ALL,mappedBy = "request")
+    @PrimaryKeyJoinColumn
+    private Review review;
 
-       @ManyToOne
-       @JoinColumn(name = "provider_id")
-       @JsonIgnore
-       private Provider provider;
+    @ManyToOne
+    @JoinColumn(name = "provider_id")
+    @JsonIgnore
+    private Provider provider;
 
-       @ManyToOne
-       @JoinColumn(name = "customer_id")
-       @JsonIgnore
-       private Customer customer;
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    @JsonIgnore
+    private Customer customer;
 
-    }
+}
 
 
